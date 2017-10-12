bae420ff8a6940f1a931cd1320b39fd117193bfe
Ingo Schommer <me@chillu.com>
2017-10-12 11:34:22 +0200

    Reordered code for clarity

--- server/mysite/code/Model/Dog.php
+++ server/mysite/code/Model/Dog.php
@@ -8,24 +8,24 @@ use MyOrg\Model\DogBreed;
 
 class Dog extends DataObject
 {
+    private static $db = [
+        'Name' => 'Varchar(255)',
+    ];
+
     private static $has_one = [
         'Owner' => Member::class,
         'Breed' => DogBreed::class,
         'Image' => Image::class
     ];
 
-    private static $db = [
-        'Name' => 'Varchar(255)',
-    ];
+    public function getThumbnail()
+    {
+        return $this->Image()->exists() ? $this->Image()->Fill(300, 300)->AbsoluteURL : null;
+    }
 
     public function canView($member = null)
     {
         return true;
-    }
-
-    public function getThumbnail()
-    {
-        return $this->Image()->exists() ? $this->Image()->Fill(300, 300)->AbsoluteURL : null;
     }
 
     public function onAfterWrite()
